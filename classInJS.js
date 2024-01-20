e7f6a9c3a3fab75a4854f889a7b0b1a19bbebe58
Abhimanyu Kanaujia <iabhimanyu.kanaujia@gmail.com>
2024-01-20 18:15:11 +0530

    Create classInJS.js

---
+++ classInJS.js
@@ -0,0 +1,17 @@
+class User {
+    constructor(name,email){
+        this.name = name;
+        this.email = email;
+    }
+
+    viewData(){
+        console.log(`website Date`)
+         
+    }
+
+}
+
+
+let user1 = new User('abhimanyu','abhi@gmail.com');
+
+console.log(user1)
