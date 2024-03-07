0ca88fa747b059103a527288f9af9e4115f4b55d
Abhimanyu Kanaujia <66407920+abhimanyu0018@users.noreply.github.com>
2024-03-07 23:31:39 +0530

    Create string_interpolation.js

---
+++ string_interpolation.js
@@ -0,0 +1,10 @@
+// template string 
+let age = 20;
+let firstName = "Abhimanyu"
+
+// "my name is Abhimanyu and my age is 20 "
+// let aboutMe = "my name is " + firstName + " and my age is " + age; 
+
+let aboutMe = `my name is ${firstName} and my age is ${age}`;
+
+console.log(aboutMe);
