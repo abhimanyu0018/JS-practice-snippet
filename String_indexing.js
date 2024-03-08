9be6f8e93614513b60e4eff6b2c7f79a9f2c5b72
Abhimanyu Kanaujia <66407920+abhimanyu0018@users.noreply.github.com>
2024-03-08 23:59:36 +0530

    Create String_indexing.js

---
+++ String_indexing.js
@@ -0,0 +1,15 @@
+// String indexing 
+
+let firstName = "abcdefg";
+
+//  a    b   c   d   e   f   g 
+//  0    1   2   3   4   5   6
+
+console.log(firstName); 
+// length of a string = stringName.length 
+// ex: firstName.length 
+console.log(firstName.length);
+
+// last Index : length - 1 
+console.log(firstName.length-1);
+console.log(firstName[firstName.length-1]);
